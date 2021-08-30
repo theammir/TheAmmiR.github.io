1df24196aecf565a707d57305f02203bfde302bc
TheAmmiR <yakorol2015@gmail.com>
2021-08-30 16:25:13 +0300

    Add another_order lance

--- json/calculate.js
+++ json/calculate.js
@@ -2,18 +2,36 @@ $(document).ready(function(){
   console.log("ready")
   $.getJSON("json/payments.json", {}, function(json) {
     let data = json.data;
+    let actual_sum = 0
+    let theory_sum = 0
+    let owe_sum = 0
     console.log(data);
 
     $.each(data.salary, function(k, v) {
+      let status_color = null;
+      if (data.lance_status[k] === "ok") {
+        status_color = "green";
+        actual_sum += v
+      } else if (data.lance_status[k] === "waiting") {
+        status_color = "yellow";
+        theory_sum += v
+      } else if (data.lance_status[k] === "did-not") {
+        status_color = "red";
+        owe_sum += v
+      }
       $("#order-table").append($("<tr>")
         .append($("<td>")
           .text(k))
         .append($("<td>")
-          .text(v + "$"))
+          .text(parseFloat(v).toFixed(2) + "$"))
         .append($("<td>")
-          .text(data.lance_status[k])));
+          .append($("<p>")
+            .attr("style", `color: ${status_color};`)
+            .text(data.lance_status[k]))));
 
     });
+
+    $("#money-sum").text(parseFloat(actual_sum).toFixed(2) + "$ + " + parseFloat(theory_sum).toFixed(2) + "$ + " + parseFloat(owe_sum).toFixed(2) + "$");
   });
 
 });
---
+++ json/manage.py
@@ -0,0 +1,36 @@
+import argparse
+import json
+import subprocess
+
+__parser = argparse.ArgumentParser()
+
+__parser.add_argument("lance_id", type=str)
+__parser.add_argument("lance_money", nargs='?', type=str)
+__parser.add_argument("lance_status", nargs="?", type=str)
+__parser.add_argument("-d", "--delete", action='store_true')
+
+
+args = __parser.parse_args()
+
+lance_status = args.lance_status
+lance_money = float(args.lance_money.strip("$"))
+if (args.lance_status == "0"):
+	lance_status = 'did-not'
+elif (args.lance_status == "1"):
+	lance_status = 'waiting'
+elif (args.lance_status == "2"):
+	lance_status = 'ok'
+
+payments = json.load(open("json/payments.json", 'r'))
+if (not args.delete):
+	payments['data']['lance_status'][args.lance_id] = lance_status
+	payments['data']['salary'][args.lance_id] = lance_money
+else:
+	del payments['data']['lance_status'][args.lance_id]
+	del payments['data']['salary'][args.lance_id]
+
+json.dump(payments, open("json/payments.json", 'w'))
+
+# Pushing to origin
+subprocess.Popen(f'git commit -am "Add {args.lance_id} lance"', shell=True)
+subprocess.Popen('git push origin master', shell=True)
